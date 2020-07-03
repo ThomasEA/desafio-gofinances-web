a95486e37645965e34ba5b5cc254b57a97b6ea0c
Everton Thomas <everton@automatizesistemas.com.br>
2020-07-03 01:45:58 -0300

    Challenge done

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,8 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    useGrouping: true,
+  }).format(value); // TODO
 
 export default formatValue;
